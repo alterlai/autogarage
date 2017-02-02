beae463443ad9273adcb2a5e4ac90eadf07e1086
Jeroen van der Laan <Jeroen.jeroenvdlaan@gmail.com>
2017-02-02 17:46:48 +0100

    pie chart

--- Autogarage/src/view/StatisticsView.java
+++ Autogarage/src/view/StatisticsView.java
@@ -3,11 +3,19 @@ package view;
 import javax.swing.*;
 import java.awt.*;
 import java.util.HashMap;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.chart.*;
+import javafx.scene.Group;
 
 import controller.*;
 
 public class StatisticsView extends View{
 	private SimulatorController controller;
+	private populationChart
 	
 	private JLabel totalCarsLabel;
 	
@@ -18,6 +26,9 @@ public class StatisticsView extends View{
 	public StatisticsView(SimulatorController controller)
 	{
 		this.controller = controller;
+		this.setLayout(new GridLayout(3, 2));
+		JPanel carPopulation = new JPanel();
+		add(carPopulation);
 		
 		totalCarsLabel = new JLabel("Total amount of cars: " + 0);
 		this.add(totalCarsLabel);
@@ -30,5 +41,33 @@ public class StatisticsView extends View{
 	public void updateView() {
 		HashMap<String, Integer> totalCarInfo = controller.getTotalCarInfo();
 		totalCarsLabel.setText("Total amount of cars: " + totalCarInfo.get("all"));
+	}
+	
+	public class PieChartSample extends Application {
+		 
+	    @Override public void start(Stage stage) {
+	        Scene scene = new Scene(new Group());
+	        stage.setTitle("Imported Fruits");
+	        stage.setWidth(500);
+	        stage.setHeight(500);
+	 
+	        ObservableList<PieChart.Data> pieChartData =
+	                FXCollections.observableArrayList(
+	                new PieChart.Data("Grapefruit", 13),
+	                new PieChart.Data("Oranges", 25),
+	                new PieChart.Data("Plums", 10),
+	                new PieChart.Data("Pears", 22),
+	                new PieChart.Data("Apples", 30));
+	        final PieChart chart = new PieChart(pieChartData);
+	        chart.setTitle("Imported Fruits");
+
+	        ((Group) scene.getRoot()).getChildren().add(chart);
+	        stage.setScene(scene);
+	        stage.show();
+	    }
+	 
+	    public static void main(String[] args) {
+	        launch(args);
+	    }
 	}
 }
